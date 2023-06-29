9ccc400e8d42bce51e93c41a3412499e7a1e32fc
Suryansh Gupta <suryanshgupta133@gmail.com>
2023-06-29 13:58:19 +0530

    Add session create and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        user = User.find_by(username: params[:session][:username])
+        # byebug
+        if user && user.authenticate(params[:session][:password])
+            # byebug
+            session[:user_id] = user.id
+            flash[:success] = "You have successfully logged in"
+            redirect_to root_path
+        else
+            # byebug
+            flash.now[:error]= "There was somethong wrong with your login information"
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        flash[:success] = "You have successfully logged out"
+        redirect_to login_path
+    end
 end
