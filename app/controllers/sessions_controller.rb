01ee8e055f1702a921f57106e4a3cbd4332dfa1a
Suryansh Gupta <suryanshgupta133@gmail.com>
2023-06-29 14:20:55 +0530

    Enable flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,7 @@ class SessionsController < ApplicationController
         else
             # byebug
             flash.now[:error]= "There was somethong wrong with your login information"
+            render 'new'
         end
     end
 
